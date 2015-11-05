dd4e347165ac49cfdc2d30e3da99ea0b0cc723ab
Your Name <Your.Name@delphi.com>
2015-11-05 14:09:16 +0800

    add py2exe support

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import py2exe
+
+wx = dict(
+    script = 'duoMainWnd.py',
+    icon_resources = [(1, "res/icon.ico")],
+)
+
+cx = dict(
+    script = 'duoMainWnd.pyw',
+    icon_resources = [(0, "res/icon.ico")],
+)
+
+setup(
+    name = "Bookload.",
+    description = "Download free book.",
+    version = "1.1.0.0",
+    # console=[cx],
+    # windows=[wx],
+    windows = [wx]
+)
